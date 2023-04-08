f78e5e16e954596bb0a616b171eda002a6dbf3b4
Stanislav F <ssfilippovich@gmail.com>
2023-04-08 15:36:09 +0300

    Добавлены переходы между страницами. Немного HTMl. Поправлены переходы со страниц пользователя после поиска товаров/просмотра. Протестирована работа функций приложения.

--- src/main/java/com/example/shopapplication/controllers/AdminController.java
+++ src/main/java/com/example/shopapplication/controllers/AdminController.java
@@ -7,7 +7,6 @@ import com.example.shopapplication.repositories.PersonRepository;
 import com.example.shopapplication.repositories.ProductRepository;
 import com.example.shopapplication.services.ProductService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/example/shopapplication/repositories/ProductRepository.java
+++ src/main/java/com/example/shopapplication/repositories/ProductRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-    // Поиск всех продуктов по части наименования продукта в не зависомости от регистра
+
     List<Product> findByTitleContainingIgnoreCase(String name);
 
-    // Поиск по наименованию и фильтрация по диапазону цены
+
     @Query(value = "select * from product where ((lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1')) and (price >= ?2 and price <= ?3)", nativeQuery = true)
     List<Product> findByTitleAndPriceGreaterThanEqualAndPriceLessThanEqual(String title, float ot, float Do);
 
